33f0269277ccffac6b894389ec083b16f1ae6477
8c6t <nkw8620@gmail.com>
2019-04-12 11:27:28 +0900

    Delete: Node 실습 소스 별도 repository 분리
    remote 경로: https://github.com/8c6t/node_textbook

--- Node/src/ch03/fs_module/copyFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile('./readme4.txt', 'writeme4.txt', (err) => {
-  if(err) { return console.error(err); }
-  console.log('복사 완료');
-});
--- Node/src/ch03/nested_object/exit.js
+++
@@ -1,9 +0,0 @@
-let i = 1;
-setInterval(() => {
-  if(i === 6) {
-    console.log('종료');
-    process.exit();
-  }
-  console.log(i);
-  i++;
-}, 1000);
